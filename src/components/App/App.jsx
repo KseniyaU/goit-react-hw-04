ce834c2e5f49ac8d3071ab6b02ef493a57e1511a
KseniyaU <140713902+KseniyaU@users.noreply.github.com>
2024-02-02 17:50:19 +0200

    4

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,22 @@
 //npm install axios
-import axios  from 'axios'
+import axios from 'axios'
 import { useState } from 'react'
 import css from '../App/App.module.css'
 import { SearchBar } from '../SearchBar/SearchBar'
-import { ImageGallery} from '../ImageGallery/ImageGallery.jsx'
+import { ImageGallery } from '../ImageGallery/ImageGallery.jsx'
+import { Loader } from '../Loader/Loader.jsx'
+import { ErrorMessage } from '../ErrorMessage/ErrorMessage.jsx'
 
 function App() {
     const key = 'CS-WUSttKITitf62ZZvU2KgNX3krMbctQMPMvCg8PHM';
     const [elements, setElements] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
 
     const searchImages = async query => {
         try {
+            setLoading(true)
+            setElements([]);
             const response =await axios.get('https://api.unsplash.com/photos/', {
                 params: { query: `${query}` },
                 headers: {
@@ -19,24 +25,22 @@ function App() {
                 
             } )
             setElements(response.data)
-            console.log(response.data);
-            //маленькі картинки
             
-            // //великі картинки
-            // console.log('вел', response.data.urls.regular)
-
         } catch(error) {
-            
+            setError(true)
+        } finally {
+            setLoading(false)
         }
         console.log(query);
     }
     return (
         <div>
             <SearchBar onSearch={searchImages}></SearchBar>
+            {loading && <Loader />}
+            { error && <ErrorMessage/>}
              <ImageGallery onElements={ elements} />
       </div>
   )
 }
 
 export default App
-//await axios.get(`https://api.unsplash.com/photos/?client_id=${key} query=${query}`);
